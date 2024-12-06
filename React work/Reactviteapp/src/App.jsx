3d9e76259f511e711cfd3cd6e4d8586a46b562d4
Varshney2309 <harshitvarshney3002@gmail.com>
2024-12-06 12:59:04 +0530

    usestate

--- React work/Reactviteapp/src/App.jsx
+++ React work/Reactviteapp/src/App.jsx
@@ -1,13 +1,48 @@
 import React from 'react'
 import Student from './Student'
+import StateHandling from './StateHandling'
 
 function App() {
+  // const studentdata= [{
+  //   image:"https://missionfinancialservices.net/wp-content/uploads/2019/04/aprilblog2.jpg",
+  //   name:"Rahul Kumar",
+  //   roll:"23456",
+  //   college:"Abes Engineering College"
+  // },
+  // {
+  //   image:"https://tse2.mm.bing.net/th?id=OIP.5F3SyVGz57QTefASUtBtpgHaHa&pid=Api&P=0&h=180",
+  //   name:"Arjun Kumar",
+  //   roll:"123456",
+  //   college:"Abes Engineering College"
+
+  // },
+  // {
+  //   image:"https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png",
+  //   name:"Harshit Kumar",
+  //   roll:"23456",
+  //   college:"Abes Engineering College"
+  // }
+  // ]
   return (
     <div>
       <div>
         <h2 style={{color:'red'}}>hello react js</h2>
         <div>
-          <Student name="Rahul Kumar" roll="2346" college="ABES Engineering college" />
+          <StateHandling />
+          
+          {/* <Student data={studentdata} /> */}
+          {/* {
+            studentdata.map((ele)=>{
+
+              return (
+                <Student data={ele} />
+
+              )
+            })
+          } */}
+        </div>
+        <div>
+          {/* <Student image={<img src='https://commondatastorage.googleapis.com/codeskulptor-demos/riceracer_assets/img/srixner.png' height={100} width={100} />} name="Rahul Kumar" roll="2346" college="ABES Engineering college" /> */}
         </div>
 
       </div>
@@ -17,3 +52,4 @@ function App() {
 }
 
 export default App
+
--- React work/Reactviteapp/src/Student.jsx
+++ React work/Reactviteapp/src/Student.jsx
@@ -1,25 +1,24 @@
 import React from 'react'
 import './style.css'
 
-function Student(props) {
+function Student({data}) {
+    // console.log(data);
   return (
     <div className='icard'>
 
         <table>
             <th style={{backgroundColor:'brown',color:'white'}}colSpan={2}>Student Information</th>
             <tbody>
-                <tr><td colSpan={2}>{props.image}</td></tr>
+                <tr><td colSpan={2}> <img src={data.image} height={150} width={150} alt="" /> </td></tr>
                 <tr>
-                    <td>Name:</td>
-                    <td>{props.name}</td>
+                    <td>Name:</td><td>{data.name}</td>
                 </tr>
                 <tr>
-                    <td>Roll:</td>
-                    <td>{props.roll}</td>
+                    <td>Roll:</td><td>{data.roll}</td>
                 </tr>
                 <tr>
                     <td>College:</td>
-                    <td>{props.college}</td>
+                    <td>{data.college}</td>
                 </tr>
 
             </tbody>
